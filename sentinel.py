820d36252d0b98cd52a54ccf41871dee12a4b044
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-09-03 21:42:37 +0800

    ff

---
+++ sentinel.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Time       : 2023/9/3 21:36
+# Author     : QIN2DIM
+# GitHub     : https://github.com/QIN2DIM
+# Description:
+import os
+from pathlib import Path
+
+if __name__ == "__main__":
+    print(os.listdir(Path(__file__).parent))
